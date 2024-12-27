52f28063d6f3d4a064ab2a87c4ec9e1cbc9e86b0
developerxsajib <sajibshikder78971@gmail.com>
2024-12-28 01:02:49 +0600

    change path formet

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const app = require("./app.js");
 const connectDatabase = require("./config/database");
+const express = require("express");
+const path = require("path");
+
+const __dirnametohid = path.resolve()
 
 // handle uncaught exceptions
 process.on('uncaughtException', (err) => {
@@ -14,10 +18,10 @@ require("dotenv").config();
 connectDatabase();
 
 if (process.env.NODE_ENV === 'PRODUCTION') {
-  app.use(express.static(path.join(__dirname, 'client', 'dist')));
+  app.use(express.static(path.join(__dirnametohid, 'client', 'dist')));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+    res.sendFile(path.join(__dirnametohid, 'client', 'dist', 'index.html'))
   );
 }
 
@@ -30,11 +34,11 @@ const server = app.listen(PORT, () => {
 
 
 
-// hander server errors
-process.on("unhandledRejection", (err) => {
-  console.log(`Error : ${err.message}`);
-  console.log(`server is shutting down due to unhandle rejection`);
-  server.close(() => {
-    process.exit(1);
-  });
-});
+// // hander server errors
+// process.on("unhandledRejection", (err) => {
+//   console.log(`Error : ${err.message}`);
+//   console.log(`server is shutting down due to unhandle rejection`);
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
